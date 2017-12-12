587f96700c58574dce53dcf7b3f76ba56b053841
lacazeto <lucas.cazeto@gmail.com>
2017-12-12 12:21:50 +0100

    added forms and data for place register

--- src/app/pages/place-creation/place-creation.component.ts
+++ src/app/pages/place-creation/place-creation.component.ts
@@ -10,10 +10,25 @@ export class PlaceCreationComponent implements OnInit {
 
   user = null;
 
+  types = [
+    { value: "Dog", display: "Dogs" },
+    { value: "Cat", display: "Cats" },
+    { value: "Any", display: "Any" },
+  ];
+
+  place = {
+    owner: "",
+    description: "",
+    isEnabled: true,
+    type: this.types[2].value
+  };
+
   constructor(private authentication: AuthenticationService) { }
 
   ngOnInit() {
     this.user = this.authentication.getUser();
   }
+
+  createPlace () {}
 
 }
